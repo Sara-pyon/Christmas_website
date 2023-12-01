5e8426fea7678c0eff69fe1d35a41df53b48394e
Sara-pyon <149462367+Sara-pyon@users.noreply.github.com>
2023-11-30 20:21:12 -0700

    Final

--- js/main.js
+++ js/main.js
@@ -63,3 +63,27 @@ const scrollActive = () =>{
 window.addEventListener('scroll', scrollActive);
 
 // ====================== PARALLAX ============================
+let parallax = new Rellax('.parallax');
+
+// ====================== GSAP ANIMATION ============================
+gsap.from('.home__village', 1.2, {opacity: 0, y: 100, delay: .1})
+gsap.from('.home__pine-tree', 1.2, {opacity: 0, y: 150, delay: .3})
+gsap.from('.home__mountain-2', 1.2, {opacity: 0, x: 150, delay: .6})
+gsap.from('.home__mountain-3', 1.2, {opacity: 0, x: -150, delay: .5})
+gsap.from('.home__mountain-1', 1.2, {opacity: 0, y: 250, delay: .7})
+gsap.from('.home__moon', 1.2, {opacity: 0, y: 200, delay: .8})
+gsap.from('.home__trineo', 1.2, {opacity: 0, x: -250, delay: .8})
+gsap.from('.home__title', 1.2, {opacity: 0, y: -30, delay: 1})
+
+// ====================== SCROLL REVERAL ANIMATION ============================
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+})
+
+sr.reveal(`.about__data`, {origin: 'right'})
+sr.reveal(`.about__image`, {origin: 'left'})
+sr.reveal(`.send__card`, {interval:100})
+sr.reveal(`.footer`)
